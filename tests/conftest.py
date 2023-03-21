e70f26697f55a23f482c8ed35b186d6d3e0eda56
Nodar Gogoberidze <gnodar01@gmail.com>
2023-03-21 10:14:11 -0400

    Create test for generate_task_definition
    - monkeypatch os.environ so that run.py does not need to be modified
    - test happy path for generating task definition

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,8 @@
-import pytest
 import os, sys
-import boto3
+from textwrap import dedent
 
+import pytest
+import boto3
 from moto import mock_sqs, mock_ecs
 
 from config import AWS_REGION
@@ -17,21 +18,73 @@ current = os.path.dirname(os.path.realpath(__file__))
 parent_directory = os.path.dirname(current)
 sys.path.append(parent_directory)
 
-@pytest.fixture(scope="function")
+# moto overrides this with 'foobar_key'
+FAKE_AWS_ACCESS_KEY_ID = 'testing'
+# FAKE_AWS_ACCESS_KEY_ID = 'DSVXIXRSGIYFOFBIDXS'
+# moto overrides this with 'foobar_secret'
+FAKE_AWS_SECRET_ACCESS_KEY = 'testing'
+# FAKE_AWS_SECRET_ACCESS_KEY = '5ZnXLgC!Ta61t6k9LlVP(mgIRHr#xlQyr&Ltfiox'
+
+FAKE_AWS_SECURITY_TOKEN = 'testing'
+FAKE_AWS_SESSION_TOKEN = 'testing'
+
+
+@pytest.fixture(autouse=True, scope="session")
 def aws_credentials():
     """Mocked AWS Credentials for moto."""
-    os.environ["AWS_ACCESS_KEY_ID"] = "testing"
-    os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
-    os.environ["AWS_SECURITY_TOKEN"] = "testing"
-    os.environ["AWS_SESSION_TOKEN"] = "testing"
+    # moto overrides these two; put here "just in case"
+    # to override moto's override, order fixtures such that
+    # these two occur strictly after moto does its stuff
+    os.environ["AWS_ACCESS_KEY_ID"] = FAKE_AWS_ACCESS_KEY_ID
+    os.environ["AWS_SECRET_ACCESS_KEY"] = FAKE_AWS_SECRET_ACCESS_KEY
+
+    os.environ["AWS_SECURITY_TOKEN"] = FAKE_AWS_SECURITY_TOKEN
+    os.environ["AWS_SESSION_TOKEN"] = FAKE_AWS_SESSION_TOKEN
+
     os.environ["AWS_DEFAULT_REGION"] = AWS_REGION
 
 @pytest.fixture(scope="function")
-def sqs(aws_credentials):
+def aws_config(monkeypatch, tmp_path):
+    """
+    Mocked AWS Config for moto.
+
+    Monkeypatches os.environ to create a fake
+    ~/.aws/config file
+    """
+    aws_dir = tmp_path / ".aws"
+    aws_dir.mkdir()
+    aws_config_file = aws_dir / "config"
+    aws_creds_file = aws_dir / "credentials"
+    
+    aws_config_file.write_text(dedent(
+        f"""
+        [default]
+        aws_access_key_id = {FAKE_AWS_ACCESS_KEY_ID}
+        aws_secret_access_key = {FAKE_AWS_SECRET_ACCESS_KEY}
+        """
+    ))
+    aws_creds_file.write_text(dedent(
+        f"""
+        [default]
+        output = json
+        region = us-east-1
+        aws_access_key_id = {FAKE_AWS_ACCESS_KEY_ID}
+        aws_secret_access_key = {FAKE_AWS_SECRET_ACCESS_KEY}
+        """
+    ))
+
+    # monkeypatch os.environ to point to our fake files
+    monkeypatch.setenv("HOME", str(tmp_path))
+
+    # return in case we want to override config or credentials file contents
+    return {"aws_dir": aws_dir, "aws_config_file": aws_config_file, "aws_creds_file": aws_creds_file}
+
+@pytest.fixture(scope="function")
+def sqs():
     with mock_sqs():
         yield boto3.client("sqs", region_name=os.environ["AWS_DEFAULT_REGION"])
 
 @pytest.fixture(scope="function")
-def ecs(aws_credentials):
+def ecs():
     with mock_ecs():
         yield boto3.client("ecs", os.environ["AWS_DEFAULT_REGION"])
--- tests/test_setup.py
+++ tests/test_setup.py
@@ -1,11 +1,12 @@
 import json
-import os
+from textwrap import dedent
 
 import pytest
 from moto import mock_sqs, mock_ecs
 
 import config
 import run
+from tests.conftest import FAKE_AWS_ACCESS_KEY_ID, FAKE_AWS_SECRET_ACCESS_KEY
 
 
 @pytest.fixture(scope="module")
@@ -23,7 +24,6 @@ class TestGetQueueURL:
     def test_get_existing_queue_url(self, sqs):
         sqs.create_queue(QueueName=self.queue_name)
         url = run.get_queue_url(sqs,self.queue_name)
-        print(url)
         assert url is not None
         assert url == f"https://sqs.{config.AWS_REGION}.amazonaws.com/123456789012/{self.queue_name}"
 
@@ -101,14 +101,59 @@ class TestGetOrCreateCluster:
         assert res["clusterArns"] == [f"arn:aws:ecs:{config.AWS_REGION}:123456789012:cluster/{config.ECS_CLUSTER}"]
 
 class TestGenerateTaskDefinition:
-    @pytest.mark.skip(reason="Not yet ready - needs to change aws credentials parsing")
-    def test_generate_task_definition(self, ecs, no_wait):
-        ...
+    def test_generate_task_definition(self, aws_config, sqs, ecs, no_wait):
+        run.get_or_create_queue(sqs)
+        run.get_or_create_cluster(ecs)
+        task_definition, taskRoleArn = run.generate_task_definition(config.AWS_PROFILE)
+
+        assert taskRoleArn == False
+
+        task_def_env = task_definition["containerDefinitions"][0]["environment"]
+        
+        aws_access_key_id_res = list(filter(
+            lambda x: x["name"] == "AWS_ACCESS_KEY_ID",
+            task_def_env
+        ))
+
+        aws_secret_access_key_res = list(filter(
+            lambda x: x["name"] == "AWS_SECRET_ACCESS_KEY", task_def_env
+        ))
+
+        queue_name_res = list(filter(
+            lambda x: x["name"] == "SQS_QUEUE_URL",
+            task_def_env
+        ))
+
+        assert len(aws_access_key_id_res) == 1
+        assert len(aws_secret_access_key_res) == 1
+        assert len(queue_name_res) == 1
+
+        assert aws_access_key_id_res[0]["value"] == FAKE_AWS_ACCESS_KEY_ID
+        assert aws_secret_access_key_res[0]["value"] == FAKE_AWS_SECRET_ACCESS_KEY
+        assert queue_name_res[0]["value"] == run.get_queue_url(sqs, config.SQS_QUEUE_NAME)
+
+    @pytest.mark.skip("Not yet implemented")
+    def test_generate_task_definition_role_arn(self, aws_config, sqs, ecs, no_wait):
+        # TODO: add role_arn option
+        aws_config['aws_config_file'].write_text(dedent(
+            f"""
+            [default]
+            aws_access_key_id = testing
+            aws_secret_access_key = testing
+            """
+        ))
+
+        run.get_or_create_queue(sqs)
+        run.get_or_create_cluster(ecs)
+        task_definition, taskRoleArn = run.generate_task_definition(config.AWS_PROFILE)
+
+        assert taskRoleArn == True
 
 class TestUpdateECSTaskDefinition:
     ecs_task_name = config.APP_NAME + 'Task'
 
     @pytest.mark.skip(reason="Not yet ready - needs to change aws credentials parsing")
+    @mock_sqs
     def test_update_ecs_task_definition(self, ecs, no_wait):
         run.update_ecs_task_definition(ecs, self.ecs_task_name, config.AWS_PROFILE)
         res = ecs.list_task_definitions()
@@ -118,6 +163,6 @@ class TestSetup:
     @mock_sqs
     @mock_ecs
     @pytest.mark.skip(reason="Not yet safe to run")
-    def test_setup(self, aws_credentials, no_wait):
+    def test_setup(self, aws_config, no_wait):
         run.setup()
         
